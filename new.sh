3a5fc206126fa5525fbbb24b14129b14c69f1a60
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-05 22:27:18 +0100

    script

--- new.sh
+++ new.sh
@@ -4,6 +4,7 @@ year=${2:-$current_year}
 num=$(printf "%02d" "$1")
 
 dotnet new aoc -q $num -y $year
+sed -i "s/https:\/\/adventofcode.com\/$year\/day\/$num/https:\/\/adventofcode.com\/$year\/day\/$1/g" "src/Pokorm.AdventOfCode/Y$year/Days/Day$num.cs"
 
 PUZZLE_URL="https://adventofcode.com/$year/day/$1/input"
 PUZZLE_FILE="src/Pokorm.AdventOfCode/Y$year/Inputs/$num.txt"
