d91a1a7586d82490e0316ac3428a02523ed63b53
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-05 12:54:13 +0100

    day 5 part 1

---
+++ src/Pokorm.AdventOfCode/Y2024/Days/Day05.cs
@@ -0,0 +1,95 @@
+﻿namespace Pokorm.AdventOfCode.Y2024.Days;
+
+// https://adventofcode.com/2024/day/5
+public class Day05
+{
+    static DayData Parse(string[] lines)
+    {
+        var rules = new HashSet<Rule>();
+        var data = new List<IReadOnlyCollection<int>>();
+        var dataRead = false;
+
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                dataRead = true;
+                continue;
+            }
+
+            if (!dataRead)
+            {
+                var numbers = line.FullSplit('|');
+
+                rules.Add(new Rule(int.Parse(numbers[0]), int.Parse(numbers[1])));
+            }
+            else
+            {
+                var numbers = line.FullSplit(',');
+
+                data.Add(numbers.Select(int.Parse).ToList());
+            }
+        }
+
+        return new DayData(rules, data);
+    }
+
+    public long Solve(string[] lines)
+    {
+        var data = Parse(lines);
+        var result = 0;
+
+        var headLookup = data.Rules.ToLookup(x => x.Head, x => x);
+
+        var tailLookup =data.Rules.ToLookup(x => x.Tail, x => x);
+
+        foreach (var line in data.Lines)
+        {
+            var seenTails = new HashSet<Rule>();
+            var correct = true;
+            
+            foreach (var num in line)
+            {
+                var heads = headLookup[num];
+
+                if (heads.Any(h => seenTails.Contains(h)))
+                {
+                    correct = false;
+                    break;
+                }
+
+                var tails = tailLookup[num];
+
+                foreach (var tail in tails)
+                {
+                    seenTails.Add(tail);
+                }
+            }
+
+            if (correct)
+            {
+                var middle = line.ElementAt((int) (line.Count / 2));
+
+                result += middle;
+            }
+        }
+        
+        return result;
+    }
+
+    public long SolveBonus(string[] lines)
+    {
+     
+        var data = Parse(lines);
+        var result = 0;
+
+        
+        return result;
+    }
+
+    private record Rule(int Head, int Tail);
+
+    private record DayData(HashSet<Rule> Rules, IReadOnlyCollection<IReadOnlyCollection<int>> Lines)
+    {
+    }
+}
---
+++ test/Pokorm.AdventOfCode.Tests/Y2024/Day05Tests.cs
@@ -0,0 +1,108 @@
+using Pokorm.AdventOfCode.Y2024.Days;
+
+namespace Pokorm.AdventOfCode.Tests.Y2024;
+
+public class Day05Tests : DayTestBase
+{
+    public Day05Tests(ITestOutputHelper output) : base(output) { }
+
+    [Fact]
+    public void SampleOne()
+    {
+        var day = new Day05();
+
+        var result = day.Solve(LinesFromSample(
+            """
+            47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+            
+            75,47,61,53,29
+            97,61,53,29,13
+            75,29,13
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47
+            """));
+
+        Assert.Equal(143, result);
+    }
+
+    [Fact]
+    public void PartOne()
+    {
+        var day = new Day05();
+
+        var result = day.Solve(LinesForDay(day));
+
+        Assert.Equal(4996, result);
+    }
+
+   [Fact]
+    public void SampleBonus()
+    {
+        var day = new Day05();
+
+        var result = day.SolveBonus(LinesFromSample(
+            """
+            47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+            
+            75,47,61,53,29
+            97,61,53,29,13
+            75,29,13
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47
+            """));
+
+        Assert.Equal(123, result);
+    }
+
+     [Fact]
+    public void PartTwo()
+    {
+        var day = new Day05();
+
+        var result = day.SolveBonus(LinesForDay(day));
+
+        Assert.Equal(1, result);
+    }
+}
