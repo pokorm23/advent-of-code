7c3246f3a4d3cdfe509fe67ea0e080b605c89279
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-06 18:19:14 +0100

    2015.6.1

--- src/Pokorm.AdventOfCode/Y2015/Days/Day06.cs
+++ src/Pokorm.AdventOfCode/Y2015/Days/Day06.cs
@@ -1,20 +1,51 @@
-﻿namespace Pokorm.AdventOfCode.Y2015.Days;
+﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+namespace Pokorm.AdventOfCode.Y2015.Days;
 
 // https://adventofcode.com/2015/day/6
-public class Day06
+public partial class Day06
 {
-    static DayData Parse(string[] lines)
+    [GeneratedRegex(@"(?<Op>turn off|turn on|toggle) (?<x1>\d{1,3}),(?<y1>\d{1,3}) through (?<x2>\d{1,3}),(?<y2>\d{1,3})")]
+    public static partial Regex AllRegex();
+
+    private static DayData Parse(string[] lines)
     {
-        return new DayData();
+        var r = AllRegex();
+        var ins = new List<Ins>();
+
+        foreach (var line in lines)
+        {
+            var match = r.Match(line);
+
+            var op = match.Groups["Op"].Value switch
+            {
+                "turn off" => Operation.TurnOff,
+                "turn on"  => Operation.TurnOn,
+                "toggle"   => Operation.Toggle,
+                var _      => throw new Exception()
+            };
+
+            var range = new Range(new Coord(int.Parse(match.Groups["x1"].Value), int.Parse(match.Groups["y1"].Value)),
+                new Coord(int.Parse(match.Groups["x2"].Value), int.Parse(match.Groups["y2"].Value)));
+
+            ins.Add(new Ins(op, range));
+        }
+
+        return new DayData(ins);
     }
 
     public long Solve(string[] lines)
     {
         var data = Parse(lines);
+        var state = State.Initial;
 
-        var result = 0;
+        foreach (var dataInstruction in data.Instructions)
+        {
+            state = state.ApplyInstruction(dataInstruction);
+        }
 
-        return result;
+        return state.Current.Count(x => x.Value);
     }
 
     public long SolveBonus(string[] lines)
@@ -26,8 +57,63 @@ public class Day06
         return result;
     }
 
-    record DayData()
+    private enum Operation
     {
-
+        TurnOn,
+        TurnOff,
+        Toggle
     }
+
+    private record struct Coord(int X, int Y);
+
+    private record struct Range(Coord A, Coord B)
+    {
+        public IEnumerable<Coord> Enumerate()
+        {
+            var x0 = Math.Min(this.A.X, this.B.X);
+            var x1 = Math.Max(this.A.X, this.B.X);
+            var y0 = Math.Min(this.A.Y, this.B.Y);
+            var y1 = Math.Max(this.A.Y, this.B.Y);
+
+            for (var x = x0; x <= x1; x++)
+            {
+                for (var y = y0; y <= y1; y++)
+                {
+                    yield return new Coord(x, y);
+                }
+            }
+        }
+    }
+
+    private record struct Ins(Operation Operation, Range Range);
+
+    private record State(IDictionary<Coord, bool> Current)
+    {
+        public static State Initial = new State(Enumerable.Range(0, 1000).SelectMany(x => Enumerable.Range(0, 1000).Select(y => new Coord(x, y)))
+                                                          .ToImmutableDictionary(c => c, _ => false));
+
+        public State ApplyInstruction(Ins ins)
+        {
+            var newCurrent = this.Current.ToDictionary();
+
+            foreach (var coord in ins.Range.Enumerate())
+            {
+                var currentState = this.Current[coord];
+
+                var newState = ins.Operation switch
+                {
+                    Operation.TurnOn  => true,
+                    Operation.TurnOff => false,
+                    Operation.Toggle  => !currentState,
+                    var _             => throw new Exception()
+                };
+
+                newCurrent[coord] = newState;
+            }
+
+            return new State(newCurrent);
+        }
+    }
+
+    private record DayData(List<Ins> Instructions) { }
 }
--- test/Pokorm.AdventOfCode.Tests/Y2015/Day06Tests.cs
+++ test/Pokorm.AdventOfCode.Tests/Y2015/Day06Tests.cs
@@ -7,16 +7,33 @@ public class Day06Tests : DayTestBase
     public Day06Tests(ITestOutputHelper output) : base(output) { }
 
     [Fact]
-    public void PartOne_Sample()
+    public void PartOne_Sample_1()
     {
         var day = new Day06();
 
-        var result = day.Solve(LinesFromSample(
-                """
-                ...
-                """));
+        var result = day.Solve([ "turn on 0,0 through 999,999" ]);
 
-        Assert.Equal(-1, result);
+        Assert.Equal(1000*1000, result);
+    }
+
+    [Fact]
+    public void PartOne_Sample_2()
+    {
+        var day = new Day06();
+
+        var result = day.Solve([ "toggle 0,0 through 999,0" ]);
+
+        Assert.Equal(1000, result);
+    }
+
+    [Fact]
+    public void PartOne_Sample_3()
+    {
+        var day = new Day06();
+
+        var result = day.Solve([ "turn off 499,499 through 500,500" ]);
+
+        Assert.Equal(0, result);
     }
 
     [Fact]
@@ -26,20 +43,47 @@ public class Day06Tests : DayTestBase
 
         var result = day.Solve(LinesForDay(day));
 
-        Assert.Equal(-1, result);
+        Assert.Equal(400410, result);
     }
 
     [Fact]
-    public void PartTwo_Sample()
+    public void PartTwo_Sample_1()
     {
         var day = new Day06();
 
-        var result = day.SolveBonus(LinesFromSample(
-                """
-                ...
-                """));
+        var result = day.SolveBonus([ "qjhvhtzxzqqjkmpb" ]);
 
-        Assert.Equal(-1, result);
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void PartTwo_Sample_2()
+    {
+        var day = new Day06();
+
+        var result = day.SolveBonus([ "xxyxx" ]);
+
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void PartTwo_Sample_3()
+    {
+        var day = new Day06();
+
+        var result = day.SolveBonus([ "uurcxstgmygtbstg" ]);
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void PartTwo_Sample_4()
+    {
+        var day = new Day06();
+
+        var result = day.SolveBonus([ "ieodomkazucvgmuy" ]);
+
+        Assert.Equal(0, result);
     }
 
     [Fact]
@@ -49,6 +93,6 @@ public class Day06Tests : DayTestBase
 
         var result = day.SolveBonus(LinesForDay(day));
 
-        Assert.Equal(-1, result);
+        Assert.Equal(69, result);
     }
 }
