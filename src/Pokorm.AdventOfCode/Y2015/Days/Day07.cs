01e39e324095e518f632ec5bd0defa18d97f461b
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-06 18:47:59 +0100

    2015.7 init

---
+++ src/Pokorm.AdventOfCode/Y2015/Days/Day07.cs
@@ -0,0 +1,33 @@
+﻿namespace Pokorm.AdventOfCode.Y2015.Days;
+
+// https://adventofcode.com/2015/day/7
+public class Day07
+{
+    static DayData Parse(string[] lines)
+    {
+        return new DayData();
+    }
+
+    public long Solve(string[] lines)
+    {
+        var data = Parse(lines);
+
+        var result = 0;
+
+        return result;
+    }
+
+    public long SolveBonus(string[] lines)
+    {
+        var data = Parse(lines);
+
+        var result = 0;
+
+        return result;
+    }
+
+    record DayData()
+    {
+
+    }
+}
