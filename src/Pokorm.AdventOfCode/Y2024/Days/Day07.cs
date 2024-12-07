253b7a3ee45a10609c01a8728b04154b43be6911
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-07 15:48:45 +0100

    2024.7.1

--- src/Pokorm.AdventOfCode/Y2024/Days/Day07.cs
+++ src/Pokorm.AdventOfCode/Y2024/Days/Day07.cs
@@ -3,16 +3,54 @@
 // https://adventofcode.com/2024/day/7
 public class Day07
 {
-    static DayData Parse(string[] lines)
+    private static DayData Parse(string[] lines)
     {
-        return new DayData();
+        var eqs = new List<Eq>();
+
+        foreach (var line in lines)
+        {
+            var s = line.FullSplit(':');
+            var other = s[1].FullSplit(' ').Select(long.Parse);
+
+            eqs.Add(new Eq(long.Parse(s[0]), other.ToList()));
+        }
+
+        return new DayData(eqs);
     }
 
     public long Solve(string[] lines)
     {
         var data = Parse(lines);
 
-        var result = 0;
+        var result = 0L;
+
+        foreach (var e in data.Equations)
+        {
+            if (Recurse(e.Test, e.Quefs))
+            {
+                result += e.Test;
+            }
+        }
+
+        static bool Recurse(long test, List<long> coefs)
+        {
+            var plusCase = coefs[0] + coefs[1];
+            var timeCase = coefs[0] * coefs[1];
+
+            if (coefs.Count == 2)
+            {
+                return plusCase == test || timeCase == test;
+            }
+
+            var result1 = Recurse(test, [ plusCase, ..coefs[2..] ]);
+
+            if (result1)
+            {
+                return result1;
+            }
+
+            return Recurse(test, [ timeCase, ..coefs[2..] ]);
+        }
 
         return result;
     }
@@ -26,8 +64,7 @@ public class Day07
         return result;
     }
 
-    record DayData()
-    {
+    private record Eq(long Test, List<long> Quefs);
 
-    }
+    private record DayData(List<Eq> Equations) { }
 }
--- test/Pokorm.AdventOfCode.Tests/Y2024/Day07Tests.cs
+++ test/Pokorm.AdventOfCode.Tests/Y2024/Day07Tests.cs
@@ -13,10 +13,18 @@ public class Day07Tests : DayTestBase
 
         var result = day.Solve(LinesFromSample(
                 """
-                ...
+                190: 10 19
+                3267: 81 40 27
+                83: 17 5
+                156: 15 6
+                7290: 6 8 6 15
+                161011: 16 10 13
+                192: 17 8 14
+                21037: 9 7 18 13
+                292: 11 6 16 20
                 """));
 
-        Assert.Equal(-1, result);
+        Assert.Equal(3749, result);
     }
 
     [Fact]
@@ -26,7 +34,8 @@ public class Day07Tests : DayTestBase
 
         var result = day.Solve(LinesForDay(day));
 
-        Assert.Equal(-1, result);
+        Assert.True(result > 2314930696600);
+        Assert.Equal(2314935962622, result);
     }
 
     [Fact]
