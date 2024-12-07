f3fb61626e4a6381c483ff849d4c812f6b64e863
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-07 19:05:41 +0100

    2015.10 init

---
+++ test/Pokorm.AdventOfCode.Tests/Y2015/Day10Tests.cs
@@ -0,0 +1,54 @@
+using Pokorm.AdventOfCode.Y2015.Days;
+
+namespace Pokorm.AdventOfCode.Tests.Y2015;
+
+public class Day10Tests : DayTestBase
+{
+    public Day10Tests(ITestOutputHelper output) : base(output) { }
+
+    [Fact]
+    public void PartOne_Sample()
+    {
+        var day = new Day10();
+
+        var result = day.Solve(LinesFromSample(
+                """
+                ...
+                """));
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void PartOne()
+    {
+        var day = new Day10();
+
+        var result = day.Solve(LinesForDay(day));
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void PartTwo_Sample()
+    {
+        var day = new Day10();
+
+        var result = day.SolveBonus(LinesFromSample(
+                """
+                ...
+                """));
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void PartTwo()
+    {
+        var day = new Day10();
+
+        var result = day.SolveBonus(LinesForDay(day));
+
+        Assert.Equal(-1, result);
+    }
+}
