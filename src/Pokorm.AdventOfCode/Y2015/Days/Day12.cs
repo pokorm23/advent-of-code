14bdf74511ade712a2c39c440055b36b747f1b92
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-08 23:32:33 +0100

    2015.12.1

--- src/Pokorm.AdventOfCode/Y2015/Days/Day12.cs
+++ src/Pokorm.AdventOfCode/Y2015/Days/Day12.cs
@@ -3,31 +3,227 @@
 // https://adventofcode.com/2015/day/12
 public class Day12
 {
-    static DayData Parse(string[] lines)
+    public long Solve(string input)
     {
-        return new DayData();
+        var data = Parse(input);
+
+        var result = 0;
+
+        var visitor = new JVisitor();
+
+        visitor.Visit(data.Root, c =>
+        {
+            result += c is JInteger i ? i.Value : 0;
+        });
+
+        return result;
     }
 
-    public long Solve(string[] lines)
+    public long SolveBonus(string input)
     {
-        var data = Parse(lines);
+        var data = Parse(input);
 
         var result = 0;
 
         return result;
     }
 
-    public long SolveBonus(string[] lines)
+    public static DayData Parse(string input)
     {
-        var data = Parse(lines);
+        var root = ParseJson(input);
 
-        var result = 0;
-
-        return result;
+        return new DayData(root);
     }
 
-    record DayData()
-    {
+    public static JToken ParseJson(string input) => ParseJToken(input).JToken;
 
+    public static (JToken JToken, string Other) ParseJToken(string input)
+    {
+        input = input.Trim();
+
+        if (input.StartsWith("["))
+        {
+            input = input[1..];
+
+            var items = new List<JToken>();
+
+            while (true)
+            {
+                (var next, input) = ParseJToken(input);
+
+                items.Add(next);
+
+                input = input.Trim();
+
+                if (input.StartsWith(","))
+                {
+                    input = input[1..];
+
+                    continue;
+                }
+
+                if (input.StartsWith("]"))
+                {
+                    input = input[1..];
+
+                    break;
+                }
+
+                throw new Exception();
+            }
+
+            return (new JArray(items), input);
+        }
+
+        if (input.StartsWith("{"))
+        {
+            input = input[1..];
+
+            var items = new Dictionary<string, JToken>();
+
+            while (true)
+            {
+                (var propNameToken, input) = ParseJToken(input);
+
+                if (propNameToken is not JString prop)
+                {
+                    throw new Exception();
+                }
+
+                input = input.Trim();
+
+                if (!input.StartsWith(":"))
+                {
+                    throw new Exception();
+                }
+
+                input = input[1..];
+
+                (var next, input) = ParseJToken(input);
+
+                items.Add(prop.Value, next);
+
+                input = input.Trim();
+
+                if (input.StartsWith(","))
+                {
+                    input = input[1..];
+
+                    continue;
+                }
+
+                if (input.StartsWith("}"))
+                {
+                    input = input[1..];
+
+                    break;
+                }
+
+                throw new Exception(input);
+            }
+
+            return (new JObject(items), input);
+        }
+
+        if (input.StartsWith("\""))
+        {
+            input = input[1..];
+
+            var text = new string(input.TakeWhile(c => c != '"').ToArray());
+
+            input = input[(text.Length + 1)..];
+
+            return (new JString(text), input);
+        }
+
+        if (input[0] == '-' || char.IsDigit(input[0]))
+        {
+            var negative = input[0] == '-';
+
+            if (negative)
+            {
+                input = input[1..];
+            }
+
+            var text = new string(input.TakeWhile(char.IsDigit).ToArray());
+
+            input = input[(text.Length - 1)..];
+
+            var value = int.Parse(text);
+
+            if (negative)
+            {
+                value = -value;
+            }
+
+            return (new JInteger(value), input[1..]);
+        }
+
+        return ParseJToken(input[1..]);
     }
+
+    public class JVisitor
+    {
+        public void Visit(JToken token, Action<JToken> action)
+        {
+            if (token is JArray a)
+            {
+                Visit(a, action);
+            }
+            else if (token is JObject b)
+            {
+                Visit(b, action);
+            }
+            else if (token is JInteger c)
+            {
+                Visit(c, action);
+            }
+            else if (token is JString d)
+            {
+                Visit(d, action);
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        private void Visit(JArray token, Action<JToken> action)
+        {
+            foreach (var tokenItem in token.Items)
+            {
+                Visit(tokenItem, action);
+            }
+        }
+
+        private void Visit(JString token, Action<JToken> action)
+        {
+            action(token);
+        }
+
+        private void Visit(JObject token, Action<JToken> action)
+        {
+            foreach (var tokenItem in token.Properties.Values)
+            {
+                Visit(tokenItem, action);
+            }
+        }
+
+        private void Visit(JInteger token, Action<JToken> action)
+        {
+            action(token);
+        }
+    }
+
+    public record JToken;
+
+    public record JArray(List<JToken> Items) : JToken;
+
+    public record JObject(Dictionary<string, JToken> Properties) : JToken;
+
+    public record JInteger(int Value) : JToken;
+
+    public record JString(string Value) : JToken;
+
+    public record DayData(JToken Root) { }
 }
--- test/Pokorm.AdventOfCode.Tests/Y2015/Day12Tests.cs
+++ test/Pokorm.AdventOfCode.Tests/Y2015/Day12Tests.cs
@@ -7,16 +7,108 @@ public class Day12Tests : DayTestBase
     public Day12Tests(ITestOutputHelper output) : base(output) { }
 
     [Fact]
-    public void PartOne_Sample()
+    public void PartOne_Parse()
     {
         var day = new Day12();
 
-        var result = day.Solve(LinesFromSample(
-                """
-                ...
-                """));
+        Assert.True(Day12.ParseJson("1") is Day12.JInteger {Value: 1});
 
-        Assert.Equal(-1, result);
+        Assert.True(Day12.ParseJson("""
+                                    "aaa "
+                                    """) is Day12.JString {Value: "aaa "});
+
+        Assert.True(Day12.ParseJson("[1,2,3]") is Day12.JArray
+        {
+            Items:
+            [
+                Day12.JInteger {Value: 1},
+                Day12.JInteger {Value: 2},
+                Day12.JInteger {Value: 3}
+            ]
+        });
+
+        Assert.True(Day12.ParseJson("""
+                                    {"a":1, "b":2}
+                                    """) is Day12.JObject
+        {
+            Properties: { } p
+        } && p.OrderBy(x => x.Key).ToList() is
+        [
+            {Key: "a", Value: Day12.JInteger {Value: 1}},
+            {Key: "b", Value: Day12.JInteger {Value: 2}}
+        ]);
+    }
+
+    [Fact]
+    public void PartOne_Sample_1()
+    {
+        var day = new Day12();
+
+        var result = day.Solve("""
+                               [1,2,3]
+                               """);
+
+        Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void PartOne_Sample_2()
+    {
+        var day = new Day12();
+
+        var result = day.Solve("""
+                               {"a":2,"b":4}
+                               """);
+
+        Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void PartOne_Sample_3()
+    {
+        var day = new Day12();
+
+        var result = day.Solve("""
+                               [[[3]]]
+                               """);
+
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void PartOne_Sample_4()
+    {
+        var day = new Day12();
+
+        var result = day.Solve("""
+                               {"a":{"b":4},"c":-1}
+                               """);
+
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void PartOne_Sample_5()
+    {
+        var day = new Day12();
+
+        var result = day.Solve("""
+                               {"a":[-1,1]}
+                               """);
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void PartOne_Sample_6()
+    {
+        var day = new Day12();
+
+        var result = day.Solve("""
+                               [-1,{"a":1}]
+                               """);
+
+        Assert.Equal(0, result);
     }
 
     [Fact]
@@ -24,31 +116,31 @@ public class Day12Tests : DayTestBase
     {
         var day = new Day12();
 
-        var result = day.Solve(LinesForDay(day));
+        var result = day.Solve(TextForDay(day));
 
-        Assert.Equal(-1, result);
+        Assert.Equal(119433, result);
     }
 
-    [Fact]
-    public void PartTwo_Sample()
-    {
-        var day = new Day12();
+    /* [Fact]
+     public void PartTwo_Sample()
+     {
+         var day = new Day12();
 
-        var result = day.SolveBonus(LinesFromSample(
-                """
-                ...
-                """));
+         var result = day.SolveBonus(LinesFromSample(
+                 """
+                 ...
+                 """));
 
-        Assert.Equal(-1, result);
-    }
+         Assert.Equal(-1, result);
+     }
 
-    [Fact]
-    public void PartTwo()
-    {
-        var day = new Day12();
+     [Fact]
+     public void PartTwo()
+     {
+         var day = new Day12();
 
-        var result = day.SolveBonus(LinesForDay(day));
+         var result = day.SolveBonus(LinesForDay(day));
 
-        Assert.Equal(-1, result);
-    }
+         Assert.Equal(-1, result);
+     }*/
 }
