598d4eb2d2387dc4d71dde3b07519afe26bd22d9
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-10 19:40:26 +0100

    2024.10.1

--- src/Pokorm.AdventOfCode/Y2024/Days/Day10.cs
+++ src/Pokorm.AdventOfCode/Y2024/Days/Day10.cs
@@ -1,18 +1,22 @@
-﻿namespace Pokorm.AdventOfCode.Y2024.Days;
+﻿using System.Collections.Concurrent;
+
+namespace Pokorm.AdventOfCode.Y2024.Days;
 
 // https://adventofcode.com/2024/day/10
 public class Day10
 {
-    static DayData Parse(string[] lines)
-    {
-        return new DayData();
-    }
-
     public long Solve(string[] lines)
     {
         var data = Parse(lines);
 
+        var heads = data.GetTrailheads();
+
         var result = 0;
+
+        foreach (var headPos in heads)
+        {
+            result += data.GetTrailCoutFromPosition(headPos);
+        }
 
         return result;
     }
@@ -26,8 +30,189 @@ public class Day10
         return result;
     }
 
-    record DayData()
+    private static BoardData Parse(string[] lines)
     {
+        var width = 0;
+        var height = lines.Length;
+        var y = 0;
+        var points = new Dictionary<Coord, Position>();
 
+        foreach (var line in lines)
+        {
+            var lineWidth = 0;
+
+            foreach (var c in line)
+            {
+                var coord = new Coord(lineWidth, y);
+
+                var num = c == '.' ? -1 : int.Parse(c.ToString());
+
+                points.Add(coord, new Position(coord, num));
+
+                lineWidth++;
+            }
+
+            width = Math.Max(width, lineWidth);
+            y++;
+        }
+
+        var board = new Size(width, height);
+
+        return new BoardData(board, points);
+    }
+
+    private record struct Vector(int X, int Y)
+    {
+        public Vector((int, int) tuple) : this(tuple.Item1, tuple.Item2) { }
+
+        public static Vector Zero = new Vector(0, 0);
+
+        public static Vector operator *(Vector c, int scale) => new Vector(c.X * scale, c.Y * scale);
+    }
+
+    private record struct Coord(int X, int Y)
+    {
+        public static Coord operator +(Coord c, Vector v) => new Coord(c.X + v.X, c.Y + v.Y);
+    }
+
+    private record Size(int Width, int Height)
+    {
+        public bool IsIn(Coord source) => source.Y >= 0 && source.X >= 0 && source.Y < this.Height && source.X < this.Width;
+    }
+
+    private record Position(Coord Coord, int Height)
+    {
+        public virtual bool Equals(Position? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return this.Coord.Equals(other.Coord);
+        }
+
+        public override int GetHashCode() => this.Coord.GetHashCode();
+    }
+
+    private record Trail(List<Position> Positions)
+    {
+        public virtual bool Equals(Trail? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return this.Positions.SequenceEqual(other.Positions);
+        }
+
+        public override int GetHashCode() => 0;
+    }
+
+    private record BoardData(Size Size, Dictionary<Coord, Position> Points)
+    {
+        public IEnumerable<Position> GetTrailheads()
+        {
+            return this.Points.Where(x => x.Value.Height == 0).Select(x => x.Value);
+        }
+
+        public int GetTrailCoutFromPosition(Position position) // BFS
+        {
+            var q = new Queue<Position>();
+
+            q.Enqueue(position);
+
+            // pos -> isOpen
+            var status = new ConcurrentDictionary<Position, bool>();
+
+            status.TryAdd(position, true);
+
+            var d = new ConcurrentDictionary<Position, int>();
+
+            var p = new ConcurrentDictionary<Position, List<Position>>();
+
+            while (q.Count > 0)
+            {
+                var v = q.Dequeue();
+
+                foreach (var w in GetNextPositions(v))
+                {
+                    if (!status.TryGetValue(w, out var _))
+                    {
+                        status.TryAdd(w, true);
+
+                        var vd = d.TryGetValue(v, out var x) ? x : 0;
+
+                        d.AddOrUpdate(w, vd + 1, (_, c) => vd + 1);
+
+                        p.AddOrUpdate(w, [ v ], (_, c) =>
+                        {
+                            c.Add(v);
+
+                            return c;
+                        });
+
+                        q.Enqueue(w);
+                    }
+                }
+
+                status.AddOrUpdate(v, false, (_, _) => false);
+            }
+
+            var full = d.Where(x => x.Value == 9)
+                        .Select(x => x.Key)
+                        .ToList();
+
+            return full.Count;
+
+            /*foreach (var position1 in full)
+            {
+                Position? pos = position1;
+
+                while (pos != position)
+                {
+                    pos = p[pos];
+                }
+            }*/
+        }
+
+        private IEnumerable<Position> GetNextPositions(Position position)
+        {
+            var dirs = new[]
+            {
+                (0, 1),
+                (1, 0),
+                (0, -1),
+                (-1, 0)
+            }.Select(x => new Vector(x));
+
+            foreach (var vector in dirs)
+            {
+                var newCoord = position.Coord + vector;
+
+                if (!this.Size.IsIn(newCoord))
+                {
+                    continue;
+                }
+
+                var pos = this.Points[newCoord];
+
+                if (pos.Height == position.Height + 1)
+                {
+                    yield return pos;
+                }
+            }
+        }
     }
 }
--- test/Pokorm.AdventOfCode.Tests/Y2024/Day10Tests.cs
+++ test/Pokorm.AdventOfCode.Tests/Y2024/Day10Tests.cs
@@ -7,16 +7,96 @@ public class Day10Tests : DayTestBase
     public Day10Tests(ITestOutputHelper output) : base(output) { }
 
     [Fact]
-    public void PartOne_Sample()
+    public void PartOne_Sample_1()
     {
         var day = new Day10();
 
         var result = day.Solve(LinesFromSample(
-                """
-                ...
-                """));
+            """
+            0123
+            1234
+            8765
+            9876
+            """));
 
-        Assert.Equal(-1, result);
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void PartOne_Sample_2()
+    {
+        var day = new Day10();
+
+        var result = day.Solve(LinesFromSample(
+            """
+            ...0...
+            ...1...
+            ...2...
+            6543456
+            7.....7
+            8.....8
+            9.....9
+            """));
+
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void PartOne_Sample_3()
+    {
+        var day = new Day10();
+
+        var result = day.Solve(LinesFromSample(
+            """
+            ..90..9
+            ...1.98
+            ...2..7
+            6543456
+            765.987
+            876....
+            987....
+            """));
+
+        Assert.Equal(4, result);
+    }
+
+    [Fact]
+    public void PartOne_Sample_4()
+    {
+        var day = new Day10();
+
+        var result = day.Solve(LinesFromSample(
+            """
+            10..9..
+            2...8..
+            3...7..
+            4567654
+            ...8..3
+            ...9..2
+            .....01
+            """));
+
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void PartOne_Sample_5()
+    {
+        var day = new Day10();
+
+        var result = day.Solve(LinesFromSample(
+            """
+            89010123
+            78121874
+            87430965
+            96549874
+            45678903
+            32019012
+            01329801
+            10456732
+            """));
+
+        Assert.Equal(36, result);
     }
 
     [Fact]
@@ -26,29 +106,29 @@ public class Day10Tests : DayTestBase
 
         var result = day.Solve(LinesForDay(day));
 
-        Assert.Equal(-1, result);
+        Assert.Equal(754, result);
     }
 
-    [Fact]
-    public void PartTwo_Sample()
-    {
-        var day = new Day10();
+    /* [Fact]
+     public void PartTwo_Sample()
+     {
+         var day = new Day10();
 
-        var result = day.SolveBonus(LinesFromSample(
-                """
-                ...
-                """));
+         var result = day.SolveBonus(LinesFromSample(
+                 """
+                 ...
+                 """));
 
-        Assert.Equal(-1, result);
-    }
+         Assert.Equal(-1, result);
+     }
 
-    [Fact]
-    public void PartTwo()
-    {
-        var day = new Day10();
+     [Fact]
+     public void PartTwo()
+     {
+         var day = new Day10();
 
-        var result = day.SolveBonus(LinesForDay(day));
+         var result = day.SolveBonus(LinesForDay(day));
 
-        Assert.Equal(-1, result);
-    }
+         Assert.Equal(-1, result);
+     }*/
 }
