22ed3b13d1e55d066f3fed19c64002b60a51a801
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-11 13:34:44 +0100

    2024.11.1

--- src/Pokorm.AdventOfCode/Y2024/Days/Day11.cs
+++ src/Pokorm.AdventOfCode/Y2024/Days/Day11.cs
@@ -1,33 +1,171 @@
-﻿namespace Pokorm.AdventOfCode.Y2024.Days;
+﻿using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace Pokorm.AdventOfCode.Y2024.Days;
 
 // https://adventofcode.com/2024/day/11
 public class Day11
 {
-    static DayData Parse(string[] lines)
+    private readonly ILogger<Day11> logger;
+
+    public Day11(ILogger<Day11> logger) => this.logger = logger;
+
+    public long Solve(string input) => SolveIterations(input, 25).Stones.Count;
+
+    public long SolveBonus(string input) => 0;
+
+    public DayData SolveIterations(string input, int iterations)
     {
-        return new DayData();
+        var origData = Parse(input);
+
+        var data = origData;
+
+        using (_ = this.logger.BeginScope($"{0:00}:"))
+        {
+            this.logger.LogDebug(data.ToString());
+        }
+
+        for (var i = 0; i < iterations; i++)
+        {
+            using var _ = this.logger.BeginScope($"{i + 1:00}:");
+
+            data = data.RunIteration();
+
+            this.logger.LogDebug(data.ToString());
+        }
+
+        this.logger.LogDebug("----------");
+
+        return data;
     }
 
-    public long Solve(string[] lines)
+    private static DayData Parse(string input)
     {
-        var data = Parse(lines);
+        var stones = input.FullSplit(' ');
 
-        var result = 0;
+        var hash = new SortedSet<Stone>();
 
-        return result;
+        var stoneStr = new StringBuilder();
+
+        var i = 0;
+
+        foreach (var s in stones)
+        {
+            hash.Add(new Stone(i)
+            {
+                Length = s.Length
+            });
+
+            stoneStr.Append(s);
+
+            i += s.Length;
+        }
+
+        return new DayData(stoneStr.ToString(), hash);
     }
 
-    public long SolveBonus(string[] lines)
+    public record struct Stone(int Offset) : IComparable<Stone>, IComparable
     {
-        var data = Parse(lines);
+        public int CompareTo(Stone other) => this.Offset.CompareTo(other.Offset);
 
-        var result = 0;
+        public int CompareTo(object? obj)
+        {
+            if (obj is null)
+            {
+                return 1;
+            }
 
-        return result;
+            return obj is Stone other ? CompareTo(other) : throw new ArgumentException($"Object must be of type {nameof(Stone)}");
+        }
+
+        public required int Length { get; init; }
+
+        public Range Range => new Range(new Index(this.Offset), new Index(this.Offset + this.Length));
     }
 
-    record DayData()
+    public record DayData(string Text, SortedSet<Stone> Stones)
     {
+        public override string ToString()
+        {
+            var result = new List<long>();
 
+            foreach (var stone in this.Stones)
+            {
+                var seg = this.Text[stone.Range];
+
+                result.Add(long.Parse(seg));
+            }
+
+            return string.Join(" ", result.Select(x => x.ToString()));
+        }
+
+        public DayData RunIteration()
+        {
+            var result = new StringBuilder(this.Text.Length);
+            var newStones = new SortedSet<Stone>();
+
+            var mem = this.Text.AsSpan();
+
+            var accOffset = 0;
+
+            foreach (var stone in this.Stones)
+            {
+                var seg = mem[stone.Range];
+
+                if (seg.Length == 1 && seg[0] == '0')
+                {
+                    result.Append('1');
+
+                    newStones.Add(stone with
+                    {
+                        Offset = stone.Offset + accOffset
+                    });
+                }
+                else if (seg.Length % 2 == 0)
+                {
+                    var mid = stone.Length / 2;
+
+                    var secondPart = seg[mid..].TrimStart('0');
+
+                    if (secondPart.Length == 0)
+                    {
+                        secondPart = [ '0' ];
+                    }
+
+                    result.Append(seg[..mid]);
+                    result.Append(secondPart);
+
+                    newStones.Add(stone with
+                    {
+                        Offset = stone.Offset + accOffset,
+                        Length = mid
+                    });
+
+                    newStones.Add(new Stone()
+                    {
+                        Offset = stone.Offset + mid + accOffset,
+                        Length = secondPart.Length
+                    });
+
+                    accOffset += secondPart.Length - mid;
+                }
+                else
+                {
+                    var newStone = (long.Parse(seg) * 2024).ToString();
+
+                    result.Append(newStone);
+
+                    newStones.Add(stone with
+                    {
+                        Offset = stone.Offset + accOffset,
+                        Length = newStone.Length
+                    });
+
+                    accOffset += newStone.Length - seg.Length;
+                }
+            }
+
+            return new DayData(result.ToString(), newStones);
+        }
     }
 }
--- test/Pokorm.AdventOfCode.Tests/Y2024/Day11Tests.cs
+++ test/Pokorm.AdventOfCode.Tests/Y2024/Day11Tests.cs
@@ -1,35 +1,72 @@
+using Microsoft.Extensions.Logging;
 using Pokorm.AdventOfCode.Y2024.Days;
 
 namespace Pokorm.AdventOfCode.Tests.Y2024;
 
 public class Day11Tests : DayTestBase
 {
-    public Day11Tests(ITestOutputHelper output) : base(output) { }
+    private readonly ILogger<Day11> logger;
+
+    public Day11Tests(ILogger<Day11> logger) => this.logger = logger;
 
     [Fact]
-    public void PartOne_Sample()
+    public void PartOne_Sample_1()
     {
-        var day = new Day11();
+        var day = new Day11(this.logger);
 
-        var result = day.Solve(LinesFromSample(
-                """
-                ...
-                """));
+        var result = day.SolveIterations("0 1 10 99 999", 1);
 
-        Assert.Equal(-1, result);
+        Assert.Equal(7, result.Stones.Count);
+        Assert.Equal("1202410992021976", result.Text);
+    }
+
+    [Fact]
+    public void PartOne_Sample_2()
+    {
+        var day = new Day11(this.logger);
+
+        var input = "125 17";
+
+        var result = day.SolveIterations(input, 1);
+
+        Assert.Equal(3, result.Stones.Count);
+
+        result = day.SolveIterations(input, 2);
+
+        Assert.Equal(4, result.Stones.Count);
+
+        result = day.SolveIterations(input, 3);
+
+        Assert.Equal(5, result.Stones.Count);
+
+        result = day.SolveIterations(input, 4);
+
+        Assert.Equal(9, result.Stones.Count);
+
+        result = day.SolveIterations(input, 5);
+
+        Assert.Equal(13, result.Stones.Count);
+
+        result = day.SolveIterations(input, 6);
+
+        Assert.Equal(22, result.Stones.Count);
+
+        result = day.SolveIterations(input, 25);
+
+        Assert.Equal(55312, result.Stones.Count);
     }
 
     [Fact]
     public void PartOne()
     {
-        var day = new Day11();
+        var day = new Day11(this.logger);
 
-        var result = day.Solve(LinesForDay(day));
+        var result = day.Solve(TextForDay(day));
 
-        Assert.Equal(-1, result);
+        Assert.Equal(218079, result);
     }
 
-    [Fact]
+    /*[Fact]
     public void PartTwo_Sample()
     {
         var day = new Day11();
@@ -50,5 +87,5 @@ public class Day11Tests : DayTestBase
         var result = day.SolveBonus(LinesForDay(day));
 
         Assert.Equal(-1, result);
-    }
+    }*/
 }
