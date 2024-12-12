694b1e701e8aa77bee7947b19d438d3265ce9b51
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-12 18:21:16 +0100

    2015.13.1

--- src/Pokorm.AdventOfCode/Y2015/Days/Day13.cs
+++ src/Pokorm.AdventOfCode/Y2015/Days/Day13.cs
@@ -1,18 +1,32 @@
-﻿namespace Pokorm.AdventOfCode.Y2015.Days;
+﻿using System.Text.RegularExpressions;
+using Microsoft.Extensions.Logging;
+
+namespace Pokorm.AdventOfCode.Y2015.Days;
 
 // https://adventofcode.com/2015/day/13
-public class Day13
+public partial class Day13
 {
-    static DayData Parse(string[] lines)
-    {
-        return new DayData();
-    }
+    private readonly ILogger<Day13> logger;
+
+    [GeneratedRegex(@"(?<name1>\w+) would (?<type>\w+) (?<amount>\d+) happiness units by sitting next to (?<name2>\w+)\.")]
+    public static partial Regex LineRegex { get; }
+
+    public Day13(ILogger<Day13> logger) => this.logger = logger;
 
     public long Solve(string[] lines)
     {
         var data = Parse(lines);
 
         var result = 0;
+
+        foreach (var s in data.GetAllSetting())
+        {
+            var total = s.Sum(x => x.AppliedSettings.Sum(y => y.Amount * (y.IsGain ? 1 : -1)));
+
+            this.logger.LogDebug($"{total}: {string.Join(", ", s.Select(x => x.Name))}");
+
+            result = Math.Max(result, total);
+        }
 
         return result;
     }
@@ -26,8 +40,82 @@ public class Day13
         return result;
     }
 
-    record DayData()
+    private static DayData Parse(string[] lines)
     {
+        var settings = new List<Setting>();
 
+        foreach (var line in lines)
+        {
+            var m = LineRegex.Match(line);
+
+            var name1 = m.Groups[1].Value;
+            var type = m.Groups[2].Value;
+            var amount = int.Parse(m.Groups[3].Value);
+            var name2 = m.Groups[4].Value;
+
+            settings.Add(new (name1, type == "gain", amount, name2));
+        }
+
+        return new DayData(settings);
+    }
+
+    private record Setting(string TargetName, bool IsGain, int Amount, string SourceName);
+
+    private record Seat(string Name, List<Setting> AppliedSettings);
+
+    private record DayData(List<Setting> Settings)
+    {
+        public IEnumerable<List<Seat>> GetAllSetting()
+        {
+            var allNames = this.Settings.Select(s => s.SourceName).Concat(this.Settings.Select(s => s.TargetName))
+                               .Distinct()
+                               .ToList();
+
+            var possibles = Get(allNames);
+
+            foreach (var possible in possibles)
+            {
+                var result = new List<Seat>();
+
+                foreach (var (i, n) in possible.Index())
+                {
+                    var left = i == 0 ? possible[^1] : possible[i - 1];
+                    var right = i == possible.Count - 1 ? possible[0] : possible[i + 1];
+
+                    var settings = new List<Setting>();
+
+                    settings.AddRange(this.Settings.Where(x => x.TargetName == n && x.SourceName == left));
+                    settings.AddRange(this.Settings.Where(x => x.TargetName == n && x.SourceName == right));
+
+                    var seat = new Seat(n, settings);
+
+                    result.Add(seat);
+                }
+
+                yield return result;
+            }
+        }
+
+        private IEnumerable<List<string>> Get(List<string> s)
+        {
+            if (s.Count <= 1)
+            {
+                yield return s;
+
+                yield break;
+            }
+
+            for (var i = 0; i < s.Count; i++)
+            {
+                var butFirst = s.Select((x, i) => (i, x)).Where(x => x.i != i).Select(x => x.x).ToList();
+
+                var subPos = Get(butFirst).ToList();
+
+                foreach (var p in subPos)
+                {
+                    yield return [ s[i], ..p ];
+                }
+            }
+        }
     }
 }
--- test/Pokorm.AdventOfCode.Tests/Y2015/Day13Tests.cs
+++ test/Pokorm.AdventOfCode.Tests/Y2015/Day13Tests.cs
@@ -1,43 +1,57 @@
+using Microsoft.Extensions.Logging;
 using Pokorm.AdventOfCode.Y2015.Days;
 
 namespace Pokorm.AdventOfCode.Tests.Y2015;
 
 public class Day13Tests : DayTestBase
 {
-    public Day13Tests(ITestOutputHelper output) : base(output) { }
+    private readonly ILogger<Day13> logger;
+
+    public Day13Tests(ILogger<Day13> logger) => this.logger = logger;
 
     [Fact]
     public void PartOne_Sample()
     {
-        var day = new Day13();
+        var day = new Day13(this.logger);
 
         var result = day.Solve(LinesFromSample(
-                """
-                ...
-                """));
+            """
+            Alice would gain 54 happiness units by sitting next to Bob.
+            Alice would lose 79 happiness units by sitting next to Carol.
+            Alice would lose 2 happiness units by sitting next to David.
+            Bob would gain 83 happiness units by sitting next to Alice.
+            Bob would lose 7 happiness units by sitting next to Carol.
+            Bob would lose 63 happiness units by sitting next to David.
+            Carol would lose 62 happiness units by sitting next to Alice.
+            Carol would gain 60 happiness units by sitting next to Bob.
+            Carol would gain 55 happiness units by sitting next to David.
+            David would gain 46 happiness units by sitting next to Alice.
+            David would lose 7 happiness units by sitting next to Bob.
+            David would gain 41 happiness units by sitting next to Carol.
+            """));
 
-        Assert.Equal(-1, result);
+        Assert.Equal(330, result);
     }
 
     [Fact]
     public void PartOne()
     {
-        var day = new Day13();
+        var day = new Day13(this.logger);
 
         var result = day.Solve(LinesForDay(day));
 
-        Assert.Equal(-1, result);
+        Assert.Equal(664, result);
     }
 
     [Fact]
     public void PartTwo_Sample()
     {
-        var day = new Day13();
+        var day = new Day13(this.logger);
 
         var result = day.SolveBonus(LinesFromSample(
-                """
-                ...
-                """));
+            """
+            ...
+            """));
 
         Assert.Equal(-1, result);
     }
@@ -45,7 +59,7 @@ public class Day13Tests : DayTestBase
     [Fact]
     public void PartTwo()
     {
-        var day = new Day13();
+        var day = new Day13(this.logger);
 
         var result = day.SolveBonus(LinesForDay(day));
 
