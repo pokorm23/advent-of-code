06ba48239b7da34053117d39abb6bec1fae0789c
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-12 19:15:20 +0100

    2015.14.1

--- src/Pokorm.AdventOfCode/Y2015/Days/Day14.cs
+++ src/Pokorm.AdventOfCode/Y2015/Days/Day14.cs
@@ -1,33 +1,129 @@
-﻿namespace Pokorm.AdventOfCode.Y2015.Days;
+﻿using System.Text.RegularExpressions;
+using Microsoft.Extensions.Logging;
+
+namespace Pokorm.AdventOfCode.Y2015.Days;
 
 // https://adventofcode.com/2015/day/14
-public class Day14
+public partial class Day14
 {
-    static DayData Parse(string[] lines)
-    {
-        return new DayData();
-    }
+    private readonly ILogger<Day14> logger;
 
-    public long Solve(string[] lines)
-    {
-        var data = Parse(lines);
+    [GeneratedRegex(@"(?<name>\w+) can fly (?<speed>\d+) km/s for (?<flyTime>\d+) seconds, but then must rest for (?<restTime>\d+) seconds\.")]
+    public static partial Regex LineRegex { get; }
 
-        var result = 0;
+    public Day14(ILogger<Day14> logger) => this.logger = logger;
 
-        return result;
-    }
+    public long Solve(string[] lines) => SolveIterations(lines, 2503);
 
-    public long SolveBonus(string[] lines)
+    public long SolveBonus(string[] lines) => SolveIterations(lines, 0);
+
+    public long SolveIterations(string[] lines, int iterations)
     {
         var data = Parse(lines);
 
-        var result = 0;
+        var state = new State(new Dictionary<Reindeer, ReindeerState>());
 
-        return result;
+        foreach (var r in data.Reindeers)
+        {
+            state.States.Add(r, new ReindeerState(Type.None, 0, 0, 0));
+        }
+
+        for (var i = 0; i < iterations; i++)
+        {
+            state = data.Run(state);
+        }
+
+        return state.States.Max(x => x.Value.Distance);
     }
 
-    record DayData()
+    private static DayData Parse(string[] lines)
     {
+        var settings = new List<Reindeer>();
 
+        foreach (var line in lines)
+        {
+            var m = LineRegex.Match(line);
+
+            var name = m.Groups[1].Value;
+            var speed = int.Parse(m.Groups[2].Value);
+            var flyTime = int.Parse(m.Groups[3].Value);
+            var restTime = int.Parse(m.Groups[4].Value);
+
+            settings.Add(new (name, speed, flyTime, restTime));
+        }
+
+        return new DayData(settings);
+    }
+
+    private record Reindeer(string Name, int Speed, int FlyTime, int RestTime);
+
+    private record State(Dictionary<Reindeer, ReindeerState> States) { }
+
+    private record ReindeerState(Type Type, int Seconds, int Distance, int TotalSeconds);
+
+    private enum Type
+    {
+        None,
+        Fly,
+        Rest
+    }
+
+    private record DayData(List<Reindeer> Reindeers)
+    {
+        public State Run(State state)
+        {
+            var newStates = new Dictionary<Reindeer, ReindeerState>();
+
+            foreach (var (r, s) in state.States)
+            {
+                var isFly = s.Type != Type.Rest;
+
+                if (isFly)
+                {
+                    if (s.Seconds >= r.FlyTime)
+                    {
+                        newStates.Add(r, s with
+                        {
+                            Type = Type.Rest,
+                            Seconds = 1,
+                            TotalSeconds = s.TotalSeconds + 1
+                        });
+                    }
+                    else
+                    {
+                        newStates.Add(r, s with
+                        {
+                            Type = Type.Fly,
+                            Seconds = s.Seconds + 1,
+                            Distance = s.Distance + r.Speed,
+                            TotalSeconds = s.TotalSeconds + 1
+                        });
+                    }
+                }
+                else
+                {
+                    if (s.Seconds >= r.RestTime)
+                    {
+                        newStates.Add(r, s with
+                        {
+                            Type = Type.Fly,
+                            Seconds = 1,
+                            Distance = s.Distance + r.Speed,
+                            TotalSeconds = s.TotalSeconds + 1
+                        });
+                    }
+                    else
+                    {
+                        newStates.Add(r, s with
+                        {
+                            Seconds = s.Seconds + 1,
+                            TotalSeconds = s.TotalSeconds + 1
+                        });
+                    }
+                }
+            }
+
+            return new State(newStates);
+        }
     }
 }
--- test/Pokorm.AdventOfCode.Tests/Y2015/Day14Tests.cs
+++ test/Pokorm.AdventOfCode.Tests/Y2015/Day14Tests.cs
@@ -1,43 +1,61 @@
+using Microsoft.Extensions.Logging;
 using Pokorm.AdventOfCode.Y2015.Days;
 
 namespace Pokorm.AdventOfCode.Tests.Y2015;
 
 public class Day14Tests : DayTestBase
 {
-    public Day14Tests(ITestOutputHelper output) : base(output) { }
+    private readonly ILogger<Day14> logger;
+
+    public Day14Tests(ILogger<Day14> logger) => this.logger = logger;
 
     [Fact]
     public void PartOne_Sample()
     {
-        var day = new Day14();
+        var day = new Day14(this.logger);
 
-        var result = day.Solve(LinesFromSample(
-                """
-                ...
-                """));
+        var result = day.SolveIterations(LinesFromSample(
+            """
+            Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+            Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+            """), 1000);
 
-        Assert.Equal(-1, result);
+        Assert.Equal(1120, result);
+    }
+
+    [Fact]
+    public void PartOne_Samples()
+    {
+        var day = new Day14(this.logger);
+
+        var result = day.SolveIterations(LinesFromSample(
+            """
+            Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+            Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+            """), 1);
+
+        Assert.Equal(16, result);
     }
 
     [Fact]
     public void PartOne()
     {
-        var day = new Day14();
+        var day = new Day14(this.logger);
 
         var result = day.Solve(LinesForDay(day));
 
-        Assert.Equal(-1, result);
+        Assert.Equal(2660, result);
     }
 
     [Fact]
     public void PartTwo_Sample()
     {
-        var day = new Day14();
+        var day = new Day14(this.logger);
 
         var result = day.SolveBonus(LinesFromSample(
-                """
-                ...
-                """));
+            """
+            ...
+            """));
 
         Assert.Equal(-1, result);
     }
@@ -45,7 +63,7 @@ public class Day14Tests : DayTestBase
     [Fact]
     public void PartTwo()
     {
-        var day = new Day14();
+        var day = new Day14(this.logger);
 
         var result = day.SolveBonus(LinesForDay(day));
 
