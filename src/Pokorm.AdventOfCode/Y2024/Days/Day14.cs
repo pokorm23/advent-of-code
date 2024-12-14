79917516406586059a10e0ab1f6c1c1b065cd39d
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-14 17:44:48 +0100

    2024.14.1

--- src/Pokorm.AdventOfCode/Y2024/Days/Day14.cs
+++ src/Pokorm.AdventOfCode/Y2024/Days/Day14.cs
@@ -1,16 +1,40 @@
 ﻿namespace Pokorm.AdventOfCode.Y2024.Days;
 
 // https://adventofcode.com/2024/day/14
-public class Day14(ILogger<Day14> logger)
+public partial class Day14(ILogger<Day14> logger)
 {
-    public long Solve(string[] lines)
+    [GeneratedRegex(@"p=(?<px>-{0,1}\d+),(?<py>-{0,1}\d+) v=(?<vx>-{0,1}\d+),(?<vy>-{0,1}\d+)")]
+    public static partial Regex RobotRegex { get; }
+
+    public long SolveInGrid(string[] lines, Grid grid, int iterations)
     {
         var data = Parse(lines);
 
-        var result = 0;
+        var state = data.Robots.ToDictionary(x => x, x => x.InitialCoord);
 
-        return result;
+        for (var i = 0; i < iterations; i++)
+        {
+            foreach (var r in data.Robots)
+            {
+                var c = state[r];
+
+                var nc = r.GetNextPosition(c, grid);
+
+                state[r] = nc;
+            }
+        }
+
+        var middle = new Coord(grid.Width / 2, grid.Height / 2);
+
+        var rq1 = state.Count(x => x.Value.X < middle.X && x.Value.Y < middle.Y);
+        var rq2 = state.Count(x => x.Value.X > middle.X && x.Value.Y < middle.Y);
+        var rq3 = state.Count(x => x.Value.X < middle.X && x.Value.Y > middle.Y);
+        var rq4 = state.Count(x => x.Value.X > middle.X && x.Value.Y > middle.Y);
+
+        return rq1 * rq2 * rq3 * rq4;
     }
+
+    public long Solve(string[] lines) => SolveInGrid(lines, new Grid(101, 103), 100);
 
     public long SolveBonus(string[] lines)
     {
@@ -21,13 +45,52 @@ public class Day14(ILogger<Day14> logger)
         return result;
     }
 
-    static DayData Parse(string[] lines)
+    private static DayData Parse(string[] lines)
     {
-        return new DayData();
+        var robots = new List<Robot>();
+
+        foreach (var line in lines)
+        {
+            var m = RobotRegex.Match(line);
+            var px = int.Parse(m.Groups["px"].Value);
+            var py = int.Parse(m.Groups["py"].Value);
+            var vx = int.Parse(m.Groups["vx"].Value);
+            var vy = int.Parse(m.Groups["vy"].Value);
+
+            robots.Add(new Robot(new Coord(px, py), new Vector(vx, vy)));
+        }
+
+        return new DayData(robots);
     }
 
-    record DayData()
+    private record Robot(Coord InitialCoord, Vector Velocity)
     {
+        public Coord GetNextPosition(Coord currentPos, Grid grid)
+        {
+            var next1 = currentPos + this.Velocity;
 
+            if (grid.IsIn(next1))
+            {
+                return next1;
+            }
+
+            var x = next1.X >= 0 && next1.X < grid.Width
+                        ? next1.X
+                        : next1.X < 0
+                            ? next1.X + grid.Width
+                            : next1.X - grid.Width;
+
+            var y = next1.Y >= 0 && next1.Y < grid.Height
+                        ? next1.Y
+                        : next1.Y < 0
+                            ? next1.Y + grid.Height
+                            : next1.Y - grid.Height;
+
+            var next2 = new Coord(x, y);
+
+            return next2;
+        }
     }
+
+    private record DayData(List<Robot> Robots) { }
 }
--- test/Pokorm.AdventOfCode.Tests/Y2024/Day14Tests.cs
+++ test/Pokorm.AdventOfCode.Tests/Y2024/Day14Tests.cs
@@ -1,3 +1,4 @@
+using Pokorm.AdventOfCode.Helpers;
 using Pokorm.AdventOfCode.Y2024.Days;
 
 namespace Pokorm.AdventOfCode.Tests.Y2024;
@@ -5,16 +6,40 @@ namespace Pokorm.AdventOfCode.Tests.Y2024;
 public class Day14Tests(ILogger<Day14> logger) : DayTestBase
 {
     [Fact]
-    public void PartOne_Sample()
+    public void PartOne_Sample_1()
     {
         var day = new Day14(logger);
 
-        var result = day.Solve(LinesFromSample(
-                """
-                ...
-                """));
+        var result = day.SolveInGrid(LinesFromSample(
+            """
+            p=2,4 v=2,-3
+            """), new Grid(11, 7), 5);
 
-        Assert.Equal(-1, result);
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void PartOne_Sample_2()
+    {
+        var day = new Day14(logger);
+
+        var result = day.SolveInGrid(LinesFromSample(
+            """
+            p=0,4 v=3,-3
+            p=6,3 v=-1,-3
+            p=10,3 v=-1,2
+            p=2,0 v=2,-1
+            p=0,0 v=1,3
+            p=3,0 v=-2,-2
+            p=7,6 v=-1,-3
+            p=3,0 v=-1,-2
+            p=9,3 v=2,3
+            p=7,3 v=-1,2
+            p=2,4 v=2,-3
+            p=9,5 v=-3,-3
+            """), new Grid(11,7), 100);
+
+        Assert.Equal(12, result);
     }
 
     [Fact]
@@ -24,7 +49,7 @@ public class Day14Tests(ILogger<Day14> logger) : DayTestBase
 
         var result = day.Solve(LinesForDay(day));
 
-        Assert.Equal(-1, result);
+        Assert.Equal(224357412, result);
     }
 
     /*[Fact]
