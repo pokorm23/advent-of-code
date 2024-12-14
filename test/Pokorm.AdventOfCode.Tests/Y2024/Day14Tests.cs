7ae37629e5c599e48a3da7f760bccb4c76f551cc
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-14 17:14:16 +0100

    2024.14 init

---
+++ test/Pokorm.AdventOfCode.Tests/Y2024/Day14Tests.cs
@@ -0,0 +1,52 @@
+using Pokorm.AdventOfCode.Y2024.Days;
+
+namespace Pokorm.AdventOfCode.Tests.Y2024;
+
+public class Day14Tests(ILogger<Day14> logger) : DayTestBase
+{
+    [Fact]
+    public void PartOne_Sample()
+    {
+        var day = new Day14(logger);
+
+        var result = day.Solve(LinesFromSample(
+                """
+                ...
+                """));
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void PartOne()
+    {
+        var day = new Day14(logger);
+
+        var result = day.Solve(LinesForDay(day));
+
+        Assert.Equal(-1, result);
+    }
+
+    /*[Fact]
+    public void PartTwo_Sample()
+    {
+        var day = new Day14(logger);
+
+        var result = day.SolveBonus(LinesFromSample(
+                """
+                ...
+                """));
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void PartTwo()
+    {
+        var day = new Day14(logger);
+
+        var result = day.SolveBonus(LinesForDay(day));
+
+        Assert.Equal(-1, result);
+    }*/
+}
