177c8c0981ac039069a2859a38eb8ec976c70ae6
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-17 23:04:23 +0100

    order

--- test/Pokorm.AdventOfCode.Tests/Usings.cs
+++ test/Pokorm.AdventOfCode.Tests/Usings.cs
@@ -1,3 +1,6 @@
 global using Xunit;
 global using Xunit.Abstractions;
 global using Microsoft.Extensions.Logging;
+using Xunit.Extensions.TestDependency;
+
+[assembly: TestCaseOrderer(DependencyOrderer.TypeName, DependencyOrderer.AssemblyName)]
--- test/Pokorm.AdventOfCode.Tests/Y2024/Day16Tests.cs
+++ test/Pokorm.AdventOfCode.Tests/Y2024/Day16Tests.cs
@@ -1,4 +1,5 @@
 using Pokorm.AdventOfCode.Y2024.Days;
+using Xunit.Extensions.TestDependency;
 
 namespace Pokorm.AdventOfCode.Tests.Y2024;
 
@@ -61,6 +62,7 @@ public class Day16Tests(ILogger<Day16> logger) : DayTestBase
     }
 
     [Fact]
+    [TestDependency(nameof(PartOne_Sample_1), nameof(PartOne_Sample_2))]
     public void PartOne()
     {
         var day = new Day16(logger);
@@ -71,33 +73,35 @@ public class Day16Tests(ILogger<Day16> logger) : DayTestBase
     }
 
     [Fact]
+    [TestDependency(nameof(PartOne))]
     public void PartTwo_Sample_1()
     {
         var day = new Day16(logger);
 
         var result = day.SolveBonus(LinesFromSample(
-                """
-                ###############
-                #.......#....E#
-                #.#.###.#.###.#
-                #.....#.#...#.#
-                #.###.#####.#.#
-                #.#.#.......#.#
-                #.#.#####.###.#
-                #...........#.#
-                ###.#.#####.#.#
-                #...#.....#.#.#
-                #.#.#.###.#.#.#
-                #.....#...#.#.#
-                #.###.#.#.#.#.#
-                #S..#.....#...#
-                ###############
-                """));
+            """
+            ###############
+            #.......#....E#
+            #.#.###.#.###.#
+            #.....#.#...#.#
+            #.###.#####.#.#
+            #.#.#.......#.#
+            #.#.#####.###.#
+            #...........#.#
+            ###.#.#####.#.#
+            #...#.....#.#.#
+            #.#.#.###.#.#.#
+            #.....#...#.#.#
+            #.###.#.#.#.#.#
+            #S..#.....#...#
+            ###############
+            """));
 
         Assert.Equal(45, result);
     }
 
     [Fact]
+    [TestDependency(nameof(PartTwo_Sample_2))]
     public void PartTwo_Sample_2()
     {
         var day = new Day16(logger);
@@ -127,6 +131,7 @@ public class Day16Tests(ILogger<Day16> logger) : DayTestBase
     }
 
     [Fact]
+    [TestDependency(nameof(PartTwo_Sample_1), nameof(PartTwo_Sample_2))]
     public void PartTwo()
     {
         var day = new Day16(logger);
