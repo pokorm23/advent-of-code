b1a26d23a999d6779cf940a59af49d14540c81c4
Michal Pokorny <michal.pokorny0@gmail.com>
2024-12-23 15:28:21 +0100

    2024.21 init

---
+++ src/Pokorm.AdventOfCode/Y2024/Days/Day21.cs
@@ -0,0 +1,33 @@
+﻿namespace Pokorm.AdventOfCode.Y2024.Days;
+
+// https://adventofcode.com/2024/day/21
+public class Day21(ILogger<Day21> logger)
+{
+    public long Solve(string[] lines)
+    {
+        var data = Parse(lines);
+
+        var result = 0;
+
+        return result;
+    }
+
+    public long SolveBonus(string[] lines)
+    {
+        var data = Parse(lines);
+
+        var result = 0;
+
+        return result;
+    }
+
+    static DayData Parse(string[] lines)
+    {
+        return new DayData();
+    }
+
+    record DayData()
+    {
+
+    }
+}
