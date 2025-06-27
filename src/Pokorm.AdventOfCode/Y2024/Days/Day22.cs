d62ee075264a650aff7344240dbc5841b806f88f
Michal Pokorny <michal.pokorny0@gmail.com>
2025-06-27 18:06:54 +0200

    2024.22.2

--- src/Pokorm.AdventOfCode/Y2024/Days/Day22.cs
+++ src/Pokorm.AdventOfCode/Y2024/Days/Day22.cs
@@ -23,9 +23,78 @@ public class Day22(ILogger<Day22> logger)
     {
         var data = Parse(lines);
 
-        var result = 0;
+        var allChanges = new List<List<(short Price, List<SecretRowSequence> Sequence)>>();
 
-        return result;
+        foreach (var secret in data.Secrets.Select(x => new SecretRow(x, GetLastDigit(x), 0)))
+        {
+            var rows = new List<SecretRow>(); // without the first
+            var changes = new List<SecretRowSequence>();
+            var cursorSecret = secret;
+
+            for (var i = 0; i < 2000; i++)
+            {
+                var nextSecretRow = GetNextSecretRow(cursorSecret);
+
+                rows.Add(nextSecretRow);
+
+                if (rows.Count > 4)
+                {
+                    var newChange = new SecretRowSequence(rows[^4], rows[^3], rows[^2], rows[^1]);
+
+                    if (!changes.Contains(newChange))
+                    {
+                        changes.Add(newChange);
+                    }
+                }
+
+                cursorSecret = nextSecretRow;
+            }
+
+            var orderedChanges = changes.GroupBy(x => x.Change4.LastDigit)
+                                        .OrderByDescending(x => x.Key)
+                                        .Select(x => (x.Key, x.Distinct().ToList()))
+                                        .ToList();
+
+            allChanges.Add(orderedChanges);
+        }
+
+        var all = allChanges.SelectMany(x => x)
+                            .SelectMany(x => x.Sequence)
+                            .ToList();
+
+        var unique = all.Distinct().ToList();
+
+        var lookup = allChanges.SelectMany((x, i) => x.Select(y => (Index: i, y.Price, y.Sequence)))
+                               .SelectMany(x => x.Sequence.Select(y => (x.Index, x.Price, Sequence: y)))
+                               .GroupBy(x => (x.Index, x.Sequence))
+                               .Select(x => (x.Key.Index, x.Key.Sequence, x.First().Price))
+                               .ToDictionary(x => (x.Index, x.Sequence), x => x.Item3);
+
+        var max = 0L;
+        SecretRowSequence? seq = null;
+
+        foreach (var ss in unique)
+        {
+            var sum = 0L;
+
+            foreach (var (i, _) in data.Secrets.Index())
+            {
+                if (!lookup.TryGetValue((i, ss), out var maxPrice))
+                {
+                    continue;
+                }
+
+                sum += maxPrice;
+            }
+
+            if (sum > max)
+            {
+                max = sum;
+                seq = ss;
+            }
+        }
+
+        return max;
     }
 
     public static long GetNextSecret(long secret, int iterations)
@@ -38,6 +107,8 @@ public class Day22(ILogger<Day22> logger)
         return secret;
     }
 
+    public static short GetLastDigit(long secret) => (short) Math.DivRem(secret, 10).Remainder;
+
     public static long GetNextSecret(long secret)
     {
         secret = Prune(Mix(secret * 64, secret));
@@ -45,6 +116,15 @@ public class Day22(ILogger<Day22> logger)
         secret = Prune(Mix(secret * 2048, secret));
 
         return secret;
+    }
+
+    public static SecretRow GetNextSecretRow(SecretRow secret)
+    {
+        var nextSecret = GetNextSecret(secret.Secret);
+        var d = GetLastDigit(nextSecret);
+        var diff = d - secret.LastDigit;
+
+        return new SecretRow(nextSecret, d, diff);
     }
 
     private static long Mix(long value, long secret) => value ^ secret;
@@ -58,5 +138,35 @@ public class Day22(ILogger<Day22> logger)
         return new DayData(secrets.ToList());
     }
 
-    private record DayData(List<long> Secrets) { }
+    private record DayData(List<long> Secrets);
+
+    public record SecretRow(long Secret, short LastDigit, int Change)
+    {
+        public override string ToString() => $"{this.Secret.ToString(),8}: {this.LastDigit} ({this.Change})";
+    }
+
+    public record SecretRowSequence(SecretRow Change1, SecretRow Change2, SecretRow Change3, SecretRow Change4)
+    {
+        public virtual bool Equals(SecretRowSequence? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return this.Change1.Change.Equals(other.Change1.Change)
+                   && this.Change2.Change.Equals(other.Change2.Change)
+                   && this.Change3.Change.Equals(other.Change3.Change)
+                   && this.Change4.Change.Equals(other.Change4.Change);
+        }
+
+        public override int GetHashCode() => HashCode.Combine(this.Change1.Change, this.Change2.Change, this.Change3.Change, this.Change4.Change);
+
+        public override string ToString() => $"{this.Change1.Change},{this.Change2.Change},{this.Change3.Change},{this.Change4.Change}";
+    }
 }
--- test/Pokorm.AdventOfCode.Tests/Y2024/Day22Tests.cs
+++ test/Pokorm.AdventOfCode.Tests/Y2024/Day22Tests.cs
@@ -10,12 +10,12 @@ public class Day22Tests(ILogger<Day22> logger) : DayTestBase
         var day = new Day22(logger);
 
         var result = day.Solve(LinesFromSample(
-                """
-                1
-                10
-                100
-                2024
-                """));
+            """
+            1
+            10
+            100
+            2024
+            """));
 
         Assert.Equal(37327623, result);
     }
@@ -30,17 +30,20 @@ public class Day22Tests(ILogger<Day22> logger) : DayTestBase
         Assert.Equal(12759339434, result);
     }
 
-    /*[Fact]
+    [Fact]
     public void PartTwo_1()
     {
         var day = new Day22(logger);
 
         var result = day.SolveBonus(LinesFromSample(
-                """
-                ...
-                """));
+            """
+            1
+            2
+            3
+            2024
+            """));
 
-        Assert.Equal(-1, result);
+        Assert.Equal(23, result);
     }
 
     [Fact]
@@ -50,6 +53,7 @@ public class Day22Tests(ILogger<Day22> logger) : DayTestBase
 
         var result = day.SolveBonus(LinesForDay(day));
 
-        Assert.Equal(-1, result);
-    }*/
+        Assert.InRange(result, 0, 1426 - 1);
+        Assert.Equal(1405, result);
+    }
 }
