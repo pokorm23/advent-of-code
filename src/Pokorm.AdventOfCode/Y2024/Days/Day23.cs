44ffdbc0e0863a4baa99eeee5f3448856bfef69d
Michal Pokorny <michal.pokorny0@gmail.com>
2025-06-28 13:58:15 +0200

    2024.23 init

---
+++ src/Pokorm.AdventOfCode/Y2024/Days/Day23.cs
@@ -0,0 +1,33 @@
+﻿namespace Pokorm.AdventOfCode.Y2024.Days;
+
+// https://adventofcode.com/2024/day/23
+public class Day23(ILogger<Day23> logger)
+{
+    public long Solve(string[] lines)
+    {
+        var data = Parse(lines);
+
+        var result = 0;
+
+        return result;
+    }
+
+    public long SolveBonus(string[] lines)
+    {
+        var data = Parse(lines);
+
+        var result = 0;
+
+        return result;
+    }
+
+    static DayData Parse(string[] lines)
+    {
+        return new DayData();
+    }
+
+    record DayData()
+    {
+
+    }
+}
---
+++ test/Pokorm.AdventOfCode.Tests/Y2024/Day23Tests.cs
@@ -0,0 +1,52 @@
+using Pokorm.AdventOfCode.Y2024.Days;
+
+namespace Pokorm.AdventOfCode.Tests.Y2024;
+
+public class Day23Tests(ILogger<Day23> logger) : DayTestBase
+{
+    [Fact]
+    public void PartOne_1()
+    {
+        var day = new Day23(logger);
+
+        var result = day.Solve(LinesFromSample(
+                """
+                ...
+                """));
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void PartOne_F()
+    {
+        var day = new Day23(logger);
+
+        var result = day.Solve(LinesForDay(day));
+
+        Assert.Equal(-1, result);
+    }
+
+    /*[Fact]
+    public void PartTwo_1()
+    {
+        var day = new Day23(logger);
+
+        var result = day.SolveBonus(LinesFromSample(
+                """
+                ...
+                """));
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void PartTwo_F()
+    {
+        var day = new Day23(logger);
+
+        var result = day.SolveBonus(LinesForDay(day));
+
+        Assert.Equal(-1, result);
+    }*/
+}
