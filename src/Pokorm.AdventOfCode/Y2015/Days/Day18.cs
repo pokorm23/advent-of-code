22ab2be7d6736584c75f5feb2f8772b2daa662fe
Michal Pokorny <michal.pokorny0@gmail.com>
2025-08-26 20:57:13 +0200

    2015.18 init

---
+++ src/Pokorm.AdventOfCode/Y2015/Days/Day18.cs
@@ -0,0 +1,33 @@
+﻿namespace Pokorm.AdventOfCode.Y2015.Days;
+
+// https://adventofcode.com/2015/day/18
+public class Day18(ILogger<Day18> logger)
+{
+    public long Solve(string[] lines)
+    {
+        var data = Parse(lines);
+
+        var result = 0;
+
+        return result;
+    }
+
+    public long SolveBonus(string[] lines)
+    {
+        var data = Parse(lines);
+
+        var result = 0;
+
+        return result;
+    }
+
+    static DayData Parse(string[] lines)
+    {
+        return new DayData();
+    }
+
+    record DayData()
+    {
+
+    }
+}
---
+++ test/Pokorm.AdventOfCode.Tests/Y2015/Day18Tests.cs
@@ -0,0 +1,56 @@
+using Pokorm.AdventOfCode.Y2015.Days;
+
+namespace Pokorm.AdventOfCode.Tests.Y2015;
+
+public class Day18Tests(ILogger<Day18> logger) : DayTestBase
+{
+    [Fact]
+    public void PartOne_1()
+    {
+        var day = new Day18(logger);
+
+        var result = day.Solve(LinesFromSample(
+                """
+                ...
+                """));
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void PartOne_F()
+    {
+        using var _ = logger.BeginScope(XUnitFormattingState.NoColor);
+
+        var day = new Day18(logger);
+
+        var result = day.Solve(LinesForDay(day));
+
+        Assert.Equal(-1, result);
+    }
+
+    /*[Fact]
+    public void PartTwo_1()
+    {
+        var day = new Day18(logger);
+
+        var result = day.SolveBonus(LinesFromSample(
+                """
+                ...
+                """));
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void PartTwo_F()
+    {
+        using var _ = logger.BeginScope(XUnitFormattingState.NoColor);
+
+        var day = new Day18(logger);
+
+        var result = day.SolveBonus(LinesForDay(day));
+
+        Assert.Equal(-1, result);
+    }*/
+}
